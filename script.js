cb07458095340f1ef0e6e336f7bc7507003a18ab
kartik Bhakre <kartikbhakre95@gmail.com>
2026-04-20 14:28:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let city = document.querySelector("#input").value ;
+let btn = document.getElementById("btn");
+btn.addEventListener('click' , data );
+
+function data(){
+    let city = document.querySelector("input").value ;  
+    let apiKey = "3414ad0badd1785e447deec7689822d4";
+    getWeatherData(city,apiKey);
+}
+
+async function getWeatherData(city,apiKey){
+   
+    try{
+         let rel = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
+    let data = await rel.json();
+    document.getElementById("weatherData").innerText = `
+    city: ${data.name}
+    ${String.fromCodePoint(127744)} temperature : ${data.main.temp}°C
+  ${String.fromCodePoint(127777)} pressure : ${data.main.pressure} Pa
+  ${String.fromCodePoint(10054)} windSpeed : ${data.wind.speed} mph
+
+    `
+    }
+    catch(err){
+        document.getElementById("weatherData").innerText = ` error ${String.fromCodePoint(128683)} ${err}` ;
+        document.querySelector("input").value = "";
+        return ;
+    
+    }
+     document.querySelector("input").value = "";
+    
+}
